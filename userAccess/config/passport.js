9f9cebac41553595c1f1c4212b193833acde50b8
ralphhanna <ralphhanna@hotmail.com>
2023-12-15 12:55:22 -0500

    Added userAccess missing files

---
+++ userAccess/config/passport.js
@@ -0,0 +1,53 @@
+const passport = require('passport');
+//const refresh = require('passport-oauth2-refresh');
+const axios = require('axios');
+const { Strategy: LocalStrategy } = require('passport-local');
+/*
+const { Strategy: FacebookStrategy } = require('passport-facebook');
+const { Strategy: SnapchatStrategy } = require('passport-snapchat');
+const { Strategy: TwitterStrategy } = require('passport-twitter');
+const { Strategy: TwitchStrategy } = require('passport-twitch-new');
+const { Strategy: GitHubStrategy } = require('passport-github2');
+const { OAuth2Strategy: GoogleStrategy } = require('passport-google-oauth');
+const { Strategy: LinkedInStrategy } = require('passport-linkedin-oauth2');
+const { Strategy: OpenIDStrategy } = require('passport-openid');
+const { OAuthStrategy } = require('passport-oauth');
+const { OAuth2Strategy } = require('passport-oauth');
+*/
+//const _ = require('lodash');
+//const moment = require('moment');
+const UserModel = require('../models/User');
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user);
+    });
+});
+/**
+ * Sign in using Email and Password.
+ */
+passport.use(new LocalStrategy({ usernameField: 'userName' }, (userName, password, done) => {
+    UserModel.findOne({ userName: userName.toLowerCase() }, (err, user) => {
+        if (err) {
+            return done(err);
+        }
+        if (!user) {
+            return done(null, false, { msg: `UserName ${userName} not found.` });
+        }
+        if (!user.password) {
+            return done(null, false, { msg: 'Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.' });
+        }
+        user.comparePassword(password, (err, isMatch) => {
+            if (err) {
+                return done(err);
+            }
+            if (isMatch) {
+                return done(null, user);
+            }
+            return done(null, false, { msg: 'Invalid email or password.' });
+        });
+    });
+}));
+//# sourceMappingURL=passport.js.map
