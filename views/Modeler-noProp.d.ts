5aa00564aef11fcbcf5fb974cbb02a34d08d8c88
lin onetwo <linonetwo012@gmail.com>
2023-12-16 19:07:13 +0800

    chore: delete useless files

--- views/Modeler-noProp.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class ModelerNoProp {
-    bpmnServer: any;
-    constructor(bpmnServer: any);
-    displayNew(name: any, request: any, response: any): Promise<void>;
-    display(process: any, request: any, response: any): Promise<void>;
-}
--- views/Modeler-wProp.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class ModelerWProp {
-    displayNew(name: any, request: any, response: any): Promise<void>;
-    display(process: any, xml: any, request: any, response: any): Promise<void>;
-}
