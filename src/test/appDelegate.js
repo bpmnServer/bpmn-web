6fa2c9a2328d5a0a1fdb16ae3fdcbb9ac979f39b
ralphhanna <ralphhanna@hotmail.com>
2023-12-21 11:48:16 -0500

    fix npm for test

---
+++ src/test/appDelegate.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TestAppDelegate = void 0;
+const appDelegate_1 = require("../WorkflowApp/appDelegate");
+class TestAppDelegate extends appDelegate_1.MyAppDelegate {
+}
+exports.TestAppDelegate = TestAppDelegate;
+//# sourceMappingURL=appDelegate.js.map
