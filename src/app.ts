5e1304e39b2897334685e9ae63f17c7d85a4eab1
ralphhanna <ralphhanna@hotmail.com>
2023-12-20 22:19:00 -0500

    fix app.ts

--- src/app.ts
+++ src/app.ts
@@ -34,7 +34,7 @@ var busboy = require('connect-busboy'); //middleware for form/file upload
 function getVersion() {
 	const fs = require('fs');
 
-	const configPath = __dirname + '/package.json';
+	const configPath = __dirname + '/../package.json';
 
 	if (fs.existsSync(configPath)) {
 
