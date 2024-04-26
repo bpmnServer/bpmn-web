f8a95b006b51b578d7e120707edd3be73cf03245
Ralph Hanna <ralphhanna@hotmail.com>
2024-04-25 23:59:11 -0400

    Release 2.2.2

--- src/test/feature/Issue197A.js
+++ src/test/feature/Issue197A.js
@@ -7,40 +7,42 @@ const logger = new Logger({ toConsole: false});
 
 const server = new BPMNServer(configuration, logger, { cron: true });
 
-let name = 'Issue197A';
+let name = 'Issue197A.txt';
 
 let process;
 let response;
 let items;
 let instances;
 let caseId = 5501;
-Feature('Cancel Loops', () => {
-
+Feature('Gateway before subprocess-Loops', () => {
+/*
         Scenario('Normal Flow', () => {
 
             Given('Start', async () => {
                 response = await server.engine.start(name, { caseId: caseId,scenario:' normal flow'});
             });
             Then('find 1 item waiting:', async () => {
+
                 items= response.instance.items;
-                let list=items.filter(i=>{return i.elementId=='Activity_create_vote'});
+                let list=items.filter(i=>{return i.elementId=='Activity_1olobi3'});
                  expect(list.length).equals(2);
             });
             Given('invoke the item:', async () => {
                 console.log('invoking');
-                response = await server.engine.invoke({"id":response.id, "items.elementId": 'Activity_create_vote',"items.itemKey":"HR" }, {"vote":"true"});
-                response = await server.engine.invoke({"id":response.id, "items.elementId": 'Activity_create_vote',"items.itemKey":"IT" }, {"vote":"true"});
+                response = await server.engine.invoke({"id":response.id, "items.elementId": 'Activity_1olobi3',"items.itemKey":"IT" }, {"vote":"true"});
+                response = await server.engine.invoke({"id":response.id, "items.elementId": 'Activity_1olobi3',"items.itemKey":"HR" }, {"vote":"true"});
             });
             Then('Summary is ready:', async () => {
-                let item=getItem('Activity_sum_votes');
+
+                let item=getItem('Activity_1mx9fl7');
                 expect(item.status).equals('wait');
             });
             Then('Invoke:', async () => {
-                response = await server.engine.invoke({"id":response.id, "items.elementId": 'Activity_sum_votes' }, {});
+                response = await server.engine.invoke({"id":response.id, "items.elementId": 'Activity_1mx9fl7' }, {});
             });
             let fileName = __dirname + '/../logs/'+name+'.log';
             Then('Summary is done:', async () => {
-                let item=getItem('Event_end1');
+                let item=getItem('Event_1jag0kh');
                 expect(item.status).equals('end');
             });
 
@@ -50,41 +52,37 @@ Feature('Cancel Loops', () => {
             }); 
 
         });
-
+*/
         Scenario('Cancel Flow', () => {
 
             Given('Start', async () => {
                 response = await server.engine.start(name, { caseId: caseId,scenario:' normal flow'});
             });
             Then('find 1 item waiting:', async () => {
+
                 items= response.instance.items;
-                let list=items.filter(i=>{return i.elementId=='Activity_create_vote'});
-                console.log(getItem('Activity_create_vote','IT').seq);
+                let list=items.filter(i=>{return i.elementId=='Activity_1olobi3'});
                  expect(list.length).equals(2);
             });
-            Given('invoke cancel:', async () => {
-                response = await server.engine.invoke({"id":response.id, "items.elementId": 'Event_cancel',"items.itemKey":"HR" }, {"vote":"true"});
+            Given('invoke the cancel:', async () => {
+                console.log('invoking');
+                response = await server.engine.invoke({"id":response.id, "items.elementId": 'Event_0qsfmsy',"items.itemKey":"IT" }, {"vote":"true"});
             });
-            Then('Delete votes ended :', async () => {
-                expect(getItem('Activity_deleteVotes').status).equals('end');
-                expect(getItem('Event_cancel','HR').status).equals('end');
-                expect(getItem('Event_cancel','IT').status).equals('end');
+            Then('All is cancelled:', async () => {
+                listItems();
+                expect(getItem('Activity_1olobi3').status).equals('end');
+                expect(getItem('Event_0qsfmsy').status).equals('end');
+                expect(getItem('Activity_1hcyu4w').status).equals('end');
+                expect(getItem('Event_1s6yeg2').status).equals('end');
             });
- 
             let fileName = __dirname + '/../logs/'+name+'.log';
-            Then('Summary is done:', async () => {
-                let item=getItem('Event_end2','HR');
-                expect(item.status).equals('end');
-            });
-
             and('write log file to ' + fileName, async () => {
                 logger.save(fileName);
-                //                console.log('filename:', __filename);
             }); 
 
-        });
+        }); 
+    });
 
-}); 
 async function delay(time) {
     log("delaying ... " + time)
     return new Promise(function (resolve) {
